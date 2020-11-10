d0cd2bb2a1a092a901d6679da237b3e74d803828
Stef Tinsley <stefaniectinsley@gmail.com>
2020-11-10 10:58:32 -0600

    Convert Trash into a Droppable'

--- assets/js/script.js
+++ assets/js/script.js
@@ -155,6 +155,22 @@ $(".card .list-group").sortable({
   },
 });
 
+$("#trash").droppable({
+  accept:".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event,ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
+
+
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function () {
   // clear values
