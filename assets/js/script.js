e4595e50aaeefb38b80ec33144745de03bf0faef
Stef Tinsley <stefaniectinsley@gmail.com>
2020-11-10 14:54:24 -0600

    Used Moment.js for due date audits

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,9 @@ var createTask = function (taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+    // check due date
+    auditTask(taskLi);
+
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
 };
@@ -123,6 +126,9 @@ $(".list-group").on("change", "input[type='text']", function () {
 
   //replace input with span elements
   $(this).replaceWith(taskSpan);
+
+  // Pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 //sortable jquery UI
@@ -168,6 +174,26 @@ $(".card .list-group").sortable({
     saveTasks();
   },
 });
+
+var auditTask = function(taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span").text().trim();
+
+  // convert to moment object at 8:00pm
+  var time = moment(date, "L").set("hour", 20);
+
+ // remove any old classes from element
+ $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+   // apply new class if task is near/over due date
+   if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  } 
+  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+
+};
 
 $("#trash").droppable({
   accept: ".card .list-group-item",
